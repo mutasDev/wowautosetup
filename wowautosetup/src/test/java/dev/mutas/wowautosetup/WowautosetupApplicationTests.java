bd79198b5f01b4ce1a8102449f92fc9a414c9b16
Markus Mutas <markus@mutas.dev>
2022-09-20 23:35:52 +0200

    Init spring backend

---
+++ wowautosetup/src/test/java/dev/mutas/wowautosetup/WowautosetupApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mutas.wowautosetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowautosetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
